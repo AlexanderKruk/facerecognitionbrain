227e8f4f20be31a0496bef4006a878bfc516ff94
AlexanderKruk <44405836+AlexanderKruk@users.noreply.github.com>
2019-02-26 14:58:31 +0300

    Add Background, Add Navigation Button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import Navigation from './components/Navigation/Navigation';
+import 'tachyons'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+      <Navigation />
+      {/*<Logo />
+      <ImageLinkForm />
+      <FaceRecognition />*/}
       </div>
     );
   }
