f351556e48d5cc82b170fdc510a4cc270b632bf7
AlexanderKruk <44405836+AlexanderKruk@users.noreply.github.com>
2019-02-26 16:51:34 +0300

    Add input form

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
-import Logo from './components/Logo/Logo'
-import 'tachyons'
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 
 class App extends Component {
   render() {
@@ -10,8 +10,8 @@ class App extends Component {
       <div className="App">
       <Navigation />
       <Logo />
-      {/*
-      <ImageLinkForm />
+      <Rank />
+      <ImageLinkForm /> {/*
       <FaceRecognition />*/}
       </div>
     );
